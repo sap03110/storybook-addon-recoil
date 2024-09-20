5d9d552d2bce8d3617ce4a9696cc131b885893da
sap03110 <wastfg6972@naver.com>
2024-09-20 16:07:39 +0900

    refactor: move stories out of src

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,10 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "./local-preset.js",
   ],
   framework: {
     name: "@storybook/react-vite",
